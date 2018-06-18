f06ba81fca5ea3406c0c793433d8a7f7dcc2201f
Matheus B. Santos <matheus.bsa@hotmail.com>
2018-06-18 18:33:50 -0300

    Documentacao

--- interfaces.h
+++ interfaces.h
@@ -9,46 +9,71 @@
 
 
 using namespace std;
-/*------------------------Interface de apresentaÁ„o de autenticaÁ„o------------------------------*/
+/*------------------------Interface de apresenta√ß√£o de autentica√ß√£o------------------------------*/
 class IaAut
+///Esta classe faz a interface que requisita
+///a funcao de logar no sistema.
+
 {
 public:
 
+///Pede ao usu√°rio o Email e a senha
+///retorna um inteiro correspondente  ao tipo de conta do usu√°rio
+///leitor, desenvolvedor ou administrador ou correspondente a uma falha no login.
     virtual int logar (Usuario *, ListaUSR *) = 0;
 
 };
 
-/*-------------------------Interface de serviÁo de autenticaÁ„o----------------------------------*/
+/*-------------------------Interface de servi√ßo de autentica√ß√£o----------------------------------*/
 
 class IsAut
+///Esta classe faz a interface que requisita
+///requisita o m√©todo que faz compara√ß√£o a ser feita entre
+///o email e senha digitados pelo usu√°rio e aqueles presentes no sistema.
+///Tal m√©todo est√° presente na classe de servi√ßo de autentica√ß√£o.
+
+
 {
 public:
 
+///Recebe um usuario, um tipo de usuario e uma lista de usuarios
+///retorna um verdadeiro se o usuario foi logado ou falso caso contrario.
     virtual bool autenticar (Usuario **, int tipoUsr,ListaUSR **) throw (runtime_error) = 0;
 
 };
 
-/*-----------------------Interface de apresentaÁ„o de gest„o de usuario---------------------------*/
+/*-----------------------Interface de apresenta√ß√£o de gest√£o de usuario---------------------------*/
 
 class IaUsr
+
 {
 public:
 
-
-    virtual void opcoesDeUsuario(Usuario*, int *, ListaUSR*)=0;
-
+///Recebe um usuario e um tipo de usuario
+///Apresenta opcoes de acoes ao usuario.
+    virtual void opcoesDeUsuario(Usuario*, int *)=0;
+    //virtual void criarConta(int) = 0;
+    //virtual void deletarUsuario(Usuario*, int*);
+    //virtual void editarDados(Usuario*, int*);
 };
 
-/*----------------------Interface de serviÁo de gest„o de usuario---------------------------------*/
+/*----------------------Interface de servi√ßo de gest√£o de usuario---------------------------------*/
 
 class IsUsr
 {
 
  public:
 
-  virtual void editarDados(Usuario**, int**,ListaUSR **) throw (runtime_error) =0;
+ ///Recebe um usuario e um tipo de usuario
+///Substitui os dados do usuario no sistema.
+  virtual void editarDados(Usuario**, int**) throw (runtime_error) =0;
+
+///Recebe um usuario e um tipo de usuario
+///Retira o cadastro do usuario do sistema.
   virtual void deletarUsuario(Usuario**, int**) throw (runtime_error) = 0;
-  virtual void criarConta(ListaUSR**) throw (runtime_error) = 0;
+
+///Armazena os dados do usuario no sistema.
+  virtual void criarConta() throw (runtime_error) = 0;
 
 };
 #endif // INTERFACES_H_INCLUDED
